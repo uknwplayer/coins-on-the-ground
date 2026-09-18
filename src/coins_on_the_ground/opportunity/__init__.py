f6c8b643221ed78337c182734a25e0471577d52b
uknwplayer <77979907+uknwplayer@users.noreply.github.com>
2026-09-18 08:20:44 -0300

    feat: expose opportunity replenishment history

--- src/coins_on_the_ground/opportunity/__init__.py
+++ src/coins_on_the_ground/opportunity/__init__.py
@@ -1,11 +1,35 @@
 from .engine import OpportunityReview, review_and_deduplicate, review_opportunity
+from .history import (
+    OpportunitySnapshot,
+    OpportunitySnapshotAppendReport,
+    ReplenishmentSignal,
+    ReplenishmentSummary,
+    ReplenishmentTransition,
+    analyze_replenishment,
+    append_opportunity_snapshots,
+    load_opportunity_snapshots,
+    make_opportunity_snapshot,
+    parse_opportunity_snapshot,
+    serialize_opportunity_snapshot,
+)
 from .model import Opportunity, OpportunityClass, RiskClass
 
 __all__ = [
     "Opportunity",
     "OpportunityClass",
     "OpportunityReview",
+    "OpportunitySnapshot",
+    "OpportunitySnapshotAppendReport",
+    "ReplenishmentSignal",
+    "ReplenishmentSummary",
+    "ReplenishmentTransition",
     "RiskClass",
+    "analyze_replenishment",
+    "append_opportunity_snapshots",
+    "load_opportunity_snapshots",
+    "make_opportunity_snapshot",
+    "parse_opportunity_snapshot",
     "review_and_deduplicate",
     "review_opportunity",
+    "serialize_opportunity_snapshot",
 ]
