c97024732b6024fb6d4f2ecd95fe4528941d4c69
uknwplayer <77979907+uknwplayer@users.noreply.github.com>
2026-09-18 05:10:33 -0300

    feat: expose global IssueHunt and Algora scouts

--- src/coins_on_the_ground/scouts/__init__.py
+++ src/coins_on_the_ground/scouts/__init__.py
@@ -1,13 +1,25 @@
+from .algora import AlgoraScout as AlgoraScout
+from .algora import parse_algora_org_html as parse_algora_org_html
 from .base import Scout as Scout
 from .frantic import FranticBountyScout as FranticBountyScout
 from .frantic import parse_frantic_issue as parse_frantic_issue
 from .github_bounties import GitHubBountyScout as GitHubBountyScout
 from .github_bounties import extract_reward as extract_reward
+from .issuehunt import IssueHuntScout as IssueHuntScout
+from .issuehunt import parse_issuehunt_html as parse_issuehunt_html
+from .sources import NetworkSurface as NetworkSurface
+from .sources import ScoutSource as ScoutSource
 
 __all__ = [
+    "AlgoraScout",
     "FranticBountyScout",
     "GitHubBountyScout",
+    "IssueHuntScout",
+    "NetworkSurface",
     "Scout",
+    "ScoutSource",
     "extract_reward",
+    "parse_algora_org_html",
     "parse_frantic_issue",
+    "parse_issuehunt_html",
 ]
