aee5a1644ff125f678eac06f6fe884bcdf7676ab
uknwplayer <77979907+uknwplayer@users.noreply.github.com>
2026-09-18 03:15:07 -0300

    feat: export GitHub bounty scout

--- src/coins_on_the_ground/scouts/__init__.py
+++ src/coins_on_the_ground/scouts/__init__.py
@@ -1,3 +1,5 @@
-from .base import Scout
+from .base import Scout as Scout
+from .github_bounties import GitHubBountyScout as GitHubBountyScout
+from .github_bounties import extract_reward as extract_reward
 
-__all__ = ["Scout"]
+__all__ = ["GitHubBountyScout", "Scout", "extract_reward"]
