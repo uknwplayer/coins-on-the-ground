240dd9c39f0633257639d8ab93d5f8c5cc5f5ccd
uknwplayer <77979907+uknwplayer@users.noreply.github.com>
2026-09-18 06:25:25 -0300

    style: sort Scout exports

--- src/coins_on_the_ground/scouts/__init__.py
+++ src/coins_on_the_ground/scouts/__init__.py
@@ -3,21 +3,20 @@ from .algora import parse_algora_org_html as parse_algora_org_html
 from .base import Scout as Scout
 from .frantic import FranticBountyScout as FranticBountyScout
 from .frantic import parse_frantic_issue as parse_frantic_issue
-from .github_bounties import GitHubBountyScout as GitHubBountyScout
 from .github_bounties import extract_reward as extract_reward
+from .github_bounties import GitHubBountyScout as GitHubBountyScout
 from .immunefi import ImmunefiScout as ImmunefiScout
 from .immunefi import parse_immunefi_listing as parse_immunefi_listing
 from .issuehunt import IssueHuntScout as IssueHuntScout
 from .issuehunt import parse_issuehunt_html as parse_issuehunt_html
-from .keep3r import Keep3rScout as Keep3rScout
 from .keep3r import discover_keep3r_jobs as discover_keep3r_jobs
-from .registry import RegisteredScoutSource as RegisteredScoutSource
-from .sherlock import SherlockScout as SherlockScout
-from .sherlock import parse_sherlock_bounties as parse_sherlock_bounties
+from .keep3r import Keep3rScout as Keep3rScout
 from .registry import parse_scout_source_registry as parse_scout_source_registry
+from .registry import RegisteredScoutSource as RegisteredScoutSource
+from .sherlock import parse_sherlock_bounties as parse_sherlock_bounties
+from .sherlock import SherlockScout as SherlockScout
 from .sources import NetworkSurface as NetworkSurface
 from .sources import ScoutSource as ScoutSource
-
 __all__ = [
     "AlgoraScout",
     "FranticBountyScout",
