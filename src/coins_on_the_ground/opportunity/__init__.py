11de84c1af901799be4e8d33156b3ec98605b393
uknwplayer <77979907+uknwplayer@users.noreply.github.com>
2026-09-18 03:27:54 -0300

    feat: export opportunity review engine

--- src/coins_on_the_ground/opportunity/__init__.py
+++ src/coins_on_the_ground/opportunity/__init__.py
@@ -1,3 +1,11 @@
+from .engine import OpportunityReview, review_and_deduplicate, review_opportunity
 from .model import Opportunity, OpportunityClass, RiskClass
 
-__all__ = ["Opportunity", "OpportunityClass", "RiskClass"]
+__all__ = [
+    "Opportunity",
+    "OpportunityClass",
+    "OpportunityReview",
+    "RiskClass",
+    "review_and_deduplicate",
+    "review_opportunity",
+]
