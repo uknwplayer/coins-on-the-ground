b5ac88ae7362462fcbe098dbc3a697ec416c6644
uknwplayer <77979907+uknwplayer@users.noreply.github.com>
2026-09-18 03:18:39 -0300

    feat: export Frantic scout

--- src/coins_on_the_ground/scouts/__init__.py
+++ src/coins_on_the_ground/scouts/__init__.py
@@ -1,5 +1,13 @@
 from .base import Scout as Scout
+from .frantic import FranticBountyScout as FranticBountyScout
+from .frantic import parse_frantic_issue as parse_frantic_issue
 from .github_bounties import GitHubBountyScout as GitHubBountyScout
 from .github_bounties import extract_reward as extract_reward
 
-__all__ = ["GitHubBountyScout", "Scout", "extract_reward"]
+__all__ = [
+    "FranticBountyScout",
+    "GitHubBountyScout",
+    "Scout",
+    "extract_reward",
+    "parse_frantic_issue",
+]
