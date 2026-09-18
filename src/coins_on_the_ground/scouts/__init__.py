ba40c5e8ae0750a70e124cad18308bfbc3c9e038
uknwplayer <77979907+uknwplayer@users.noreply.github.com>
2026-09-18 05:12:01 -0300

    feat: expose global scout source registry

--- src/coins_on_the_ground/scouts/__init__.py
+++ src/coins_on_the_ground/scouts/__init__.py
@@ -7,6 +7,8 @@ from .github_bounties import GitHubBountyScout as GitHubBountyScout
 from .github_bounties import extract_reward as extract_reward
 from .issuehunt import IssueHuntScout as IssueHuntScout
 from .issuehunt import parse_issuehunt_html as parse_issuehunt_html
+from .registry import RegisteredScoutSource as RegisteredScoutSource
+from .registry import parse_scout_source_registry as parse_scout_source_registry
 from .sources import NetworkSurface as NetworkSurface
 from .sources import ScoutSource as ScoutSource
 
@@ -16,10 +18,12 @@ __all__ = [
     "GitHubBountyScout",
     "IssueHuntScout",
     "NetworkSurface",
+    "RegisteredScoutSource",
     "Scout",
     "ScoutSource",
     "extract_reward",
     "parse_algora_org_html",
     "parse_frantic_issue",
     "parse_issuehunt_html",
+    "parse_scout_source_registry",
 ]
