20c9683e6454f4069a30031f19765c30679d1520
uknwplayer <77979907+uknwplayer@users.noreply.github.com>
2026-09-18 03:36:14 -0300

    feat: expose feasibility estimator

---
+++ src/coins_on_the_ground/estimation/__init__.py
@@ -0,0 +1,17 @@
+from .estimator import estimate_feasibility
+from .model import (
+    Capability,
+    CapabilityProfile,
+    FeasibilityClass,
+    FeasibilityEstimate,
+    ProfitabilityClass,
+)
+
+__all__ = [
+    "Capability",
+    "CapabilityProfile",
+    "FeasibilityClass",
+    "FeasibilityEstimate",
+    "ProfitabilityClass",
+    "estimate_feasibility",
+]
