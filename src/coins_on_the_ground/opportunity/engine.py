6a61722a63668d65aab1cdf2cf7f3f610cb53308
uknwplayer <77979907+uknwplayer@users.noreply.github.com>
2026-09-23 10:03:03 -0300

    Score Averray public job contract highly

--- src/coins_on_the_ground/opportunity/engine.py
+++ src/coins_on_the_ground/opportunity/engine.py
@@ -9,6 +9,7 @@ from coins_on_the_ground.opportunity.model import Opportunity, RiskClass
 from coins_on_the_ground.policies import evaluate_for_review
 
 _SOURCE_EVIDENCE_SCORE = {
+    "averray": 99,
     "agent-bounties": 98,
     "bidpostloop": 96,
     "clawlancer": 97,
